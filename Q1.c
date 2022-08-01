347e89d080ad2df22de5c7b41e36d7ea1f87f9c3
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-08-01 17:46:08 +0530

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void n_NaturalNumbers(int);
+  
+int main()  
+{  
+   int lt;
+   printf("Enter limit:\n");
+   scanf("%d",&lt);
+   n_NaturalNumbers(lt);
+   
+  
+    return 0;  
+}  
+void n_NaturalNumbers(int k)
+{
+    
+    if(k>0){
+        n_NaturalNumbers(k-1);
+    }
+    else return;
+  printf("%d\n",k);
+    
+}
